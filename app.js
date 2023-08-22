4cbf659b9becd5f3b5e6bf19a85df6243f46e1d2
rigadira <rigadira@gmail.com>
2023-08-22 13:09:47 +0300

    Change

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
-//Start express app
+//Start express application
 const app = express();
 
 app.set('view engine', 'pug');
